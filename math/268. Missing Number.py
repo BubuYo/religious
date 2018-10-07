b6c6a24abeaf58fbc48d78bbec5ae49b14402b23
刘金俭 <liujinjian@zhihu.com>
2018-10-07 23:27:31 +0800

    add math topic

---
+++ math/268. Missing Number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(range(len(nums) + 1)) - sum(nums)
