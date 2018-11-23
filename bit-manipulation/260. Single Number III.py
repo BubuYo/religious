83dc07d4df9e15b80e2f7ffe93f6194030173d05
liujinjian <liujinjian@zhihu.com>
2018-11-23 20:28:48 +0800

    add 260. Single Number III

--- bit-manipulation/260. Single Number III.py
+++ bit-manipulation/260. Single Number III.py
@@ -5,3 +5,18 @@ class Solution(object):
         :rtype: List[int]
         """
         return [k for k, v in collections.Counter(nums).items() if v == 1]
+
+
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = set()
+        for i in nums:
+            if i in ans:
+                ans.remove(i)
+            else:
+                ans.add(i)
+        return list(ans)
