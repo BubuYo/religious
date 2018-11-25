e1b9a7c12fbde90c8b2087fa86470791b2cb531d
liujinjian <liujinjian@zhihu.com>
2018-11-25 16:29:25 +0800

    :zap: add 55. Jump Game

--- array/55. Jump Game.py
+++ array/55. Jump Game.py
@@ -4,18 +4,11 @@ class Solution:
         :type nums: List[int]
         :rtype: bool
         """
-        l, remain = len(nums), 0
+        length, remain = len(nums), 0
         for idx, i in enumerate(nums):
-            if idx + i + 1 >= l:
+            if idx + i + 1 >= length:
                 return True
             remain = max(i, remain - 1)
             if not any([i, remain]):
                 return False
         return True
-
-
-if __name__ == '__main__':
-    s = Solution()
-    nums = [3, 2, 1, 0, 4]
-    a = s.canJump(nums)
-    print(a)
