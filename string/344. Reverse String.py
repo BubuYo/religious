49fb54a91ef809e329d61162df19fe871bac0d98
liujinjian <liujinjian@zhihu.com>
2018-12-02 14:10:12 +0800

    🙄

--- string/344. Reverse String.py
+++ string/344. Reverse String.py
@@ -5,3 +5,15 @@ class Solution:
         :rtype: str
         """
         return s[::-1]
+
+
+class Solution:
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        l1 = list(s)
+        for i in range(len(s) // 2):
+            l1[i], l1[len(s) - 1 - i] = l1[len(s) - 1 - i], l1[i]
+        return ''.join(l1)
