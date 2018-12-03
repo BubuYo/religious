1703fee72d88c162e92a08874574171cbfa47a8f
liujinjian <liujinjian@zhihu.com>
2018-12-03 23:13:33 +0800

    🙄

--- string/345. Reverse Vowels of a String.py
+++ string/345. Reverse Vowels of a String.py
@@ -16,3 +16,15 @@ class Solution:
             l += 1
             r -= 1
         return ''.join(s)
+
+
+class Solution:
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        tmp, indexs = list(s), [idx for idx, i in enumerate(s) if i in 'aeiouAEIOU']
+        for i in range(len(indexs) // 2):
+            tmp[indexs[i]], tmp[indexs[-i - 1]] = tmp[indexs[-i - 1]], tmp[indexs[i]]
+        return ''.join(tmp)
