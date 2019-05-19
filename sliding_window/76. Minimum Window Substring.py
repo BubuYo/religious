769ac5c061f6308840e4c3aa32da2a571aac66f8
Bubuyo <liujinjian@zhihu.com>
2019-05-19 20:57:35 +0800

    add sliding window

---
+++ sliding_window/76. Minimum Window Substring.py
@@ -0,0 +1,23 @@
+import collections
+
+
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        need, missing = collections.Counter(t), len(t)
+        left = I = J = 0
+        for right, c in enumerate(s, 1):
+            missing -= need[c] > 0
+            need[c] -= 1
+            if missing == 0:
+                while left < right and need[s[left]] < 0:
+                    need[s[left]] += 1
+                    left += 1
+                if J == 0 or right - left <= J - I:
+                    I, J = left, right
+        return s[I:J]
+
