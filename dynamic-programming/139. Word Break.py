589a9715dbd7b75ba4b7bbabaa16ebd0d5e3320d
liujinjian <liujinjian@zhihu.com>
2019-11-30 21:46:31 +0800

    add comment

--- dynamic-programming/139. Word Break.py
+++ dynamic-programming/139. Word Break.py
@@ -1,6 +1,6 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # dp is uesd to record if s[:index] can break
+        # dp is uesd to record if s[:index] can break.
         dp = [1] + [0] * len(s)
         for j in range(len(s) + 1):
             for k in range(j):
