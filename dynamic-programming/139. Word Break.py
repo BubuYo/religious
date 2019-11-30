51d51beef021cb814dbbb7eb1477dfef1fbfae6f
liujinjian <liujinjian@zhihu.com>
2019-11-30 21:14:19 +0800

    dp +1

---
+++ dynamic-programming/139. Word Break.py
@@ -0,0 +1,8 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [1] + [0] * len(s)
+        for j in range(len(s) + 1):
+            for k in range(j):
+                if dp[k] and s[k:j] in wordDict:
+                    dp[j] = True
+        return dp[-1]
