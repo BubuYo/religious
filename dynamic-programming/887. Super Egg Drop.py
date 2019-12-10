1cfa1b6427f4509342ca8b8cd48e63ef01dd3d9a
liujinjian <liujinjian@zhihu.com>
2019-12-10 09:24:42 +0800

    super egg drop version 2

--- dynamic-programming/887. Super Egg Drop.py
+++ dynamic-programming/887. Super Egg Drop.py
@@ -22,3 +22,13 @@ class Solution:
                 if dp[K][step] >= N:
                     return step
         return 0
+
+class Solution2:
+    def superEggDrop(self, K: int, N: int) -> int:
+        dp = [0 for _ in range(K + 1)]
+        res = 0
+        while dp[K] < N:
+            for i in range(K, 0, -1):
+                dp[i] = dp[i] + dp[i - 1] + 1
+            res += 1
+        return res
