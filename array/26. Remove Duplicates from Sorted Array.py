0fe46c6627cf78789cca0ac0a862bae5d3fd4ff3
Bubuyo <liujinjian@zhihu.com>
2021-01-14 22:54:43 +0800

    update 26

--- array/26. Remove Duplicates from Sorted Array.py
+++ array/26. Remove Duplicates from Sorted Array.py
@@ -4,6 +4,15 @@ class Solution:
         :type nums: List[int]
         :rtype: int
         """
-        nums[:] = list(set(nums))
-        nums.sort()
-        return len(nums)
+        # only put unique num within nums[:i+1]
+        # other num not remove to make it into O(1)
+        if not nums:
+            return 0
+        i = 0  # slow pointer
+        for j in range(1, len(nums)):
+            # j: quick pointer
+            if nums[i] != nums[j]:
+                i += 1
+                nums[i] = nums[j]
+                print(nums)
+        return i + 1
