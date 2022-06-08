2fde343aa0ab021d23b652a180d9e51706a96ade
liujinjian <liujinjian@taou.com>
2022-06-08 16:10:47 +0800

    dfs

--- dfs/216. Combination Sum III.py
+++ dfs/216. Combination Sum III.py
@@ -1,17 +1,15 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res = []
-        self.dfs(range(1, 10), k, n, 0, [], res)
+        self.dfs(range(1, 10), k, n, [], res)
         return res
 
-    def dfs(self, nums, k, n, index, path, res):
-        if k < 0 or n < 0:
+    def dfs(self, nums, k, n, path, res):
+        if n < 0:
             return
 
         if k == 0 and n == 0:
             res.append(path)
 
         for idx, num in enumerate(nums):
-            if idx < index:
-                continue
-            self.dfs(nums, k - 1, n - num, idx + 1, path + [num], res)
+            self.dfs(nums[idx+1:], k - 1, n - num, path + [num], res)
