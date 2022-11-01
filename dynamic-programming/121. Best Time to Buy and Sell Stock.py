d53c001be9301bbbc6248b50ab91571bb0b32172
liujinjian <liujinjian@taou.com>
2022-11-01 17:46:03 +0800

    stock

--- dynamic-programming/121. Best Time to Buy and Sell Stock.py
+++ dynamic-programming/121. Best Time to Buy and Sell Stock.py
@@ -4,14 +4,47 @@ class Solution:
         :type prices: List[int]
         :rtype: int
         """
-        if not prices:
+        if len(prices) < 2:
             return 0
-        ans = 0
-        low = prices[0]
-        # 向后遍历一遍价格
+        p0 = 0
+        p1 = -prices[0]
         for i in range(1, len(prices)):
-            # 记录目前位置的最低价格
-            low = min(low, prices[i])
-            # 当前新数值减去当前最低价
-            ans = max(ans, prices[i] - low)
-        return ans
+            p0 = max(p0, p1 + prices[i])
+            p1 = max(p1, -prices[i])
+        return p0
+
+
+'''
+状态转移方程
+T[i][k][0] = max(T[i - 1][k][0], T[i - 1][k][1] + prices[i])
+T[i][k][1] = max(T[i - 1][k][1], T[i - 1][k - 1][0] - prices[i])
+
+
+这一题 k=1
+T[i][1][0] = max(T[i - 1][1][0], T[i - 1][1][1] + prices[i])
+T[i][1][1] = max(T[i - 1][1][1], T[i - 1][0][0] - prices[i]) = max(T[i - 1][1][1], -prices[i])
+
+
+
+第一版 删掉k
+if len(prices)<2:
+    return 0
+
+l = len(prices)
+dp = [[0 for _ in range(l)] for _ in range(l)]
+dp[0][0] = 0
+dp[0][1] = -prices[0]
+for i in range(1, l):
+    dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i])
+    dp[i][1] = max(dp[i - 1][1], -prices[i])
+return dp[l - 1][0]
+
+
+第二版 删掉 i
+p0=0
+p1=-prices[0]
+for i in range(1, len(prices)):
+    p0 = max(p0, p1 + prices[i])
+    p1 = max(p1, -prices[i])
+return p0
+'''
--- dynamic-programming/123. Best Time to Buy and Sell Stock III.py
+++ dynamic-programming/123. Best Time to Buy and Sell Stock III.py
@@ -13,27 +13,4 @@ class Solution:
         return sum(ans[:2])  # 失败，因为出现新情况：1324时，13 24 两次截取, 但是因为只能截取两次交易，所以只能是 14，不能只取到 13 和 24
 
 
-class Solution2:
-    def maxProfit(self, prices) -> int:
-        n = len(prices)
-        left = [0] * n
-        right = [0] * n
-        minl = prices[0]
-        maxr = prices[-1]
-        for i in range(1, n):
-            minl = min(minl, prices[i])
-            left[i] = max(left[i - 1], prices[i] - minl)
-            j = n - i - 1
-            maxr = max(maxr, prices[j])
-            right[j] = max(right[j + 1], maxr - prices[j])
-        print(left, right)
 
-        profit = right[0]
-        for i in range(1, n):
-            profit = max(profit, left[i - 1] + right[i])
-        return profit
-
-
-if __name__ == '__main__':
-    a = Solution2()
-    a.maxProfit([3, 3, 5, 0, 0, 3, 1, 4])
