b33b353b0087f356d8047d90473acda900804002
liujinjian <liujinjian@taou.com>
2022-11-01 17:59:31 +0800

    stock

--- dynamic-programming/121. Best Time to Buy and Sell Stock.py
+++ dynamic-programming/121. Best Time to Buy and Sell Stock.py
@@ -24,9 +24,11 @@ T[i][k][1] = max(T[i - 1][k][1], T[i - 1][k - 1][0] - prices[i])
 T[i][1][0] = max(T[i - 1][1][0], T[i - 1][1][1] + prices[i])
 T[i][1][1] = max(T[i - 1][1][1], T[i - 1][0][0] - prices[i]) = max(T[i - 1][1][1], -prices[i])
 
+然后 k=1，常数直接砍掉 k
+T[i][0] = max(T[i - 1][0], T[i - 1][1] + prices[i])
+T[i][1] = max(T[i - 1][1], -prices[i])
 
-
-第一版 删掉k
+第一版 
 if len(prices)<2:
     return 0
 
@@ -40,7 +42,10 @@ for i in range(1, l):
 return dp[l - 1][0]
 
 
-第二版 删掉 i
+第二版 
+第i天的最大收益只和第i - 1天的最大收益相关
+p1 记录到 i 时的最小值（负数然后取最大 相当于取最小）
+p0 记录当前 i 处价格减去当前的 p1 的值，最终留下最大的 p0
 p0=0
 p1=-prices[0]
 for i in range(1, len(prices)):
